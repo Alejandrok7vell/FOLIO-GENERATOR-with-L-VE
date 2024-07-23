f2970b1a14fb490f44aa74e64020ca01319889e5
Alejandro Hernandez <79550879+Alejandrok7vell@users.noreply.github.com>
2024-07-23 07:55:11 -0600

    Add files via upload

--- conf.lua
+++ conf.lua
@@ -1,3 +1,9 @@
-function love.conf(c)
-    c.console = true
+function love.conf(t)
+    t.console = false
+    t.window.title = "Creador de Folios - Professional Plus Business Enterprise Edition 2024 uwu"         -- The window title (string)
+    t.window.icon = "logo.png"                 -- Filepath to an image to use as the window's icon (string)
+    t.window.width = 700             -- The window width (number)
+    t.window.height = 368               -- The window height (number)
+    t.window.borderless = false         -- Remove all border visuals from the window (boolean)
+    t.window.resizable = false          -- Let the window be user-resizable (boolean)
 end
--- main.lua
+++ main.lua
@@ -3,30 +3,43 @@ local utf8 = require"utf8"
 function love.load()
     keys = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}
     fase = 1
-    textos = { "Por favor escriba el PRIMER FOLIO c:", "Por favor escriba el ÚLTIMO FOLIO :D", "¿Por cuánto los divido, gefe? ;)", "Listo uwu, /n el archivo quedó guardado en %APPDATA%/love" }
+    textos = { "Por favor escriba el PRIMER FOLIO c:", "Porfis escriba el ÚLTIMO FOLIO :D", "¿Por cuánto los divido? ;)", "Listo uwu, \n el archivo quedó guardado en %APPDATA%/love" }
     data = {}
     text = ""
     ceros = 0
     ztxt = ""
+    video = love.graphics.newVideo("background.ogg")
+    video:play()
+    bg = love.graphics.newImage("bg1.png")
+    fonts = { text = love.graphics.newFont("fonts/Lilita_One/LilitaOne-Regular.ttf", 31), num = love.graphics.newFont("fonts/OSWALD/Oswald-Bold.ttf", 125)}
 end
 
 function love.update()
     if love.keyboard.isDown("escape") then love.event.quit() end
+
+    if not video:isPlaying() then video:rewind() end
 end
 
 function love.draw()
-    love.graphics.print(textos[fase])
-    love.graphics.print(text, 50, 50)
+   love.graphics.setColor(1,1,1)
+   love.graphics.draw(video, 0, 0, nil, 0.4, 0.4)
+   love.graphics.draw(bg)
+
+   love.graphics.setFont(fonts.text)
+   love.graphics.print(textos[fase], 40, 30)
+   love.graphics.setFont(fonts.num)
+    love.graphics.print(text, 40, 175)
+    
 end
 
 function love.textinput(t)  for i = 1, 10, 1 do
-    if t == keys[i] then text = text .. t end
+    if t == keys[i] and string.len(text) < 7 then text = text .. t end
     end
 end
 
 function printFolios(n1, n2, div)
     local fLong = ((n2-n1) + 1) / div
-    f = love.filesystem.newFile("FOLIOS "..ztxt.." al "..n2..".txt")
+    f = love.filesystem.newFile("FOLIOS "..ztxt.." al "..n2.." - x"..div..".txt")
     f:open("w")
 
     for i = 1, div, 1 do
